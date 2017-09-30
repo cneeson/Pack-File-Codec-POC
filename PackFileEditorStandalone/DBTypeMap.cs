7bbb447b27fc9004146559c9171365cf344e0429
Sean Kennedy <sean-kennedy1191@hotmail.co.uk>
2017-09-30 16:12:04 +0100

    Added git ignore
    Added files folder from original project
    
    Refactored DBTypeMap so that we can control when it gets all type infos, removed singleton pattern and introduced static class to handle type infos
    
    GetDataTable still return an empty DataTable, didn't bother mapping the PackedFile to the data table but the PackedFile gets populated now.

--- PackFileEditorStandalone/DBTypeMap.cs
+++ PackFileEditorStandalone/DBTypeMap.cs
@@ -16,36 +16,58 @@ namespace PackFileEditorStandalone
      * they are encoded as, therefore often several type infos may be applicable for a given file
      * without reading the whole table.
      */
-    public class DBTypeMap : IEnumerable<TypeInfo> {
+    public static class DBTypeMap
+    {
         public static readonly string MASTER_SCHEMA_FILE_NAME = "master_schema.xml";
         public static readonly string SCHEMA_USER_FILE_NAME = "schema_user.xml";
         public static readonly string MODEL_SCHEMA_FILE_NAME = "schema_models.xml";
-  
-        List<TypeInfo> typeInfos = new List<TypeInfo>();
-        public List<TypeInfo> AllInfos {
-            get {
+
+        static List<TypeInfo> typeInfos = new List<TypeInfo>();
+
+        public static void InitializeAllTypeInfos(string basePath)
+        {
+
+            foreach (string file in SCHEMA_FILENAMES)
+            {
+                string xmlFile = Path.Combine(basePath, file);
+                if (File.Exists(xmlFile))
+                {
+                    XmlImporter importer = null;
+                    using (Stream stream = File.OpenRead(xmlFile))
+                    {
+                        importer = new XmlImporter(stream);
+                        importer.Import(true);
+                    }
+                    typeInfos = importer.Imported;
+                    if (File.Exists(MODEL_SCHEMA_FILE_NAME))
+                    {
+                        importer = null;
+                        using (Stream stream = File.OpenRead(MODEL_SCHEMA_FILE_NAME))
+                        {
+                            importer = new XmlImporter(stream);
+                            importer.Import();
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        public static List<TypeInfo> AllInfos
+        {
+            get
+            {
                 return typeInfos;
             }
         }
 
         /*
-         * Singleton access.
-         */
-        static readonly DBTypeMap instance = new DBTypeMap();        
-        public static DBTypeMap Instance {
-            get {
-                return instance;
-            }
-        }
-        private DBTypeMap() {
-            // prevent instantiation
-        }
-        
-        /*
          * Query if any schema has been loaded.
          */
-        public bool Initialized {
-            get {
+        public static bool Initialized
+        {
+            get
+            {
                 return typeInfos.Count != 0;
             }
         }
@@ -64,11 +86,15 @@ namespace PackFileEditorStandalone
          * Retrieve all infos currently loaded for the given table,
          * either in the table/version format or from the GUID list.
          */
-        public List<TypeInfo> GetAllInfos(string table) {
+        public static List<TypeInfo> GetAllInfos(string table)
+        {
             List<TypeInfo> result = new List<TypeInfo>();
-            typeInfos.ForEach(t => { if (t.Name.Equals(table)) { 
-                    result.Add (t);
-                }});
+            typeInfos.ForEach(t => {
+                if (t.Name.Equals(table))
+                {
+                    result.Add(t);
+                }
+            });
             return result;
         }
         /*
@@ -76,7 +102,8 @@ namespace PackFileEditorStandalone
          * There may be more than one because sometimes, there are several GUIDs with
          * the same type/version but different structures.
          */
-        public List<TypeInfo> GetVersionedInfos(string key, int version) {
+        public static List<TypeInfo> GetVersionedInfos(string key, int version)
+        {
             List<TypeInfo> result = new List<TypeInfo>(GetAllInfos(key));
             result.Sort(new BestVersionComparer { TargetVersion = version });
 #if DEBUG
@@ -89,48 +116,54 @@ namespace PackFileEditorStandalone
         /*
          * Read schema from given directory, in the order of the SCHEMA_FILENAMES.
          */
-        public void InitializeTypeMap(string basePath) {
-            foreach(string file in SCHEMA_FILENAMES) {
-                string xmlFile = Path.Combine(basePath, file);
-                if (File.Exists(xmlFile)) {
-                    initializeFromFile(xmlFile);
-                    break;
-                }
-            }
-        }
+        //public static void InitializeTypeMap(string basePath) {
+        //    foreach(string file in SCHEMA_FILENAMES) {
+        //        string xmlFile = Path.Combine(basePath, file);
+        //        if (File.Exists(xmlFile)) {
+        //            initializeFromFile(xmlFile);
+        //            break;
+        //        }
+        //    }
+        //}
         /*
          * Load the given schema xml file.
          */
-        public void initializeFromFile(string filename) {
-            XmlImporter importer = null;
-            using (Stream stream = File.OpenRead(filename)) {
-                importer = new XmlImporter(stream);
-                importer.Import(true);
-            }
-            typeInfos = importer.Imported;
-            if (File.Exists(MODEL_SCHEMA_FILE_NAME)) {
-                importer = null;
-                using (Stream stream = File.OpenRead(MODEL_SCHEMA_FILE_NAME)) {
-                    importer = new XmlImporter(stream);
-                    importer.Import();
-                }
-            }
-        }
+        //public void initializeFromFile(string filename) {
+        //    XmlImporter importer = null;
+        //    using (Stream stream = File.OpenRead(filename)) {
+        //        importer = new XmlImporter(stream);
+        //        importer.Import(true);
+        //    }
+        //    typeInfos = importer.Imported;
+        //    if (File.Exists(MODEL_SCHEMA_FILE_NAME)) {
+        //        importer = null;
+        //        using (Stream stream = File.OpenRead(MODEL_SCHEMA_FILE_NAME)) {
+        //            importer = new XmlImporter(stream);
+        //            importer.Import();
+        //        }
+        //    }
+        //}
+
         /*
          * Stores the whole schema to a file at the given directory with the given suffix.
          */
-        public void SaveToFile(string path, string suffix) {
+        public static void SaveToFile(string path, string suffix)
+        {
             string filename = Path.Combine(path, GetUserFilename(suffix));
             string backupName = filename + ".bak";
-            if (File.Exists(filename)) {
+            if (File.Exists(filename))
+            {
                 File.Copy(filename, backupName);
             }
             SaveToFile(filename);
-            if (File.Exists(backupName)) {
+            if (File.Exists(backupName))
+            {
                 File.Delete(backupName);
             }
         }
-        public void SaveToFile(string filename) {
+
+        public static void SaveToFile(string filename)
+        {
 #if DEBUG
             Console.WriteLine("saving schema file {0}", filename);
 #endif
@@ -139,76 +172,76 @@ namespace PackFileEditorStandalone
             stream.Close();
         }
         #endregion
-  
+
         #region Setting Changed Definitions
-        public void SetByName(string key, List<FieldInfo> setTo) {
-            typeInfos.Add(new TypeInfo(setTo) {
+        public static void SetByName(string key, List<FieldInfo> setTo)
+        {
+            typeInfos.Add(new TypeInfo(setTo)
+            {
                 Name = key
             });
         }
         #endregion
 
-        #region Utilities
-        public string GetUserFilename(string suffix) {
+        public static string GetUserFilename(string suffix)
+        {
             return string.Format(string.Format("schema_{0}.xml", suffix));
         }
-        #endregion
 
-        #region Supported Type/Version Queries
         /*
+         * Supported Type/Version Queries
          * Retrieve all supported Type Names.
          */
-        public List<string> DBFileTypes {
-            get {
+        public static List<string> DBFileTypes
+        {
+            get
+            {
                 SortedSet<string> result = new SortedSet<string>();
-                typeInfos.ForEach(t => { if (!result.Contains(t.Name)) { 
-                    result.Add(t.Name);
-                }});  
+                typeInfos.ForEach(t => {
+                    if (!result.Contains(t.Name))
+                    {
+                        result.Add(t.Name);
+                    }
+                });
                 return new List<string>(result);
             }
         }
-  
+
         /*
          * Retrieve the highest version for the given type.
          */
-        public int MaxVersion(string type) {
+        public static int MaxVersion(string type)
+        {
             int result = 0;
             typeInfos.ForEach(t => { if (t.Name == type) { result = Math.Max(t.Version, result); } });
             return result;
         }
+
         /*
          * Query if the given type is supported at all.
          */
-        public bool IsSupported(string type) {
-            foreach(TypeInfo info in typeInfos) {
-                if (info.Name.Equals(type)) {
+        public static bool IsSupported(string type)
+        {
+            foreach (TypeInfo info in typeInfos)
+            {
+                if (info.Name.Equals(type))
+                {
                     return true;
                 }
             }
             return false;
         }
-        #endregion
-        
-        /*
-         * Note:
-         * The names of the TypeInfos iterated here cannot be changed using the
-         * enumeration; the FieldInfo lists and contained FieldInfos can.
-         */
-        public IEnumerator<TypeInfo> GetEnumerator() {
-            return typeInfos.GetEnumerator();
-        }
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
-            return GetEnumerator();
-        }
     }
-    
+
     /*
      * Class defining a db type by GUID. They do still carry their type name
      * and a version number along; the name/version tuple may not be unique though.
      */
-    public class GuidTypeInfo : IComparable<GuidTypeInfo> {
-        public GuidTypeInfo(string guid) : this(guid, "", 0) {}
-        public GuidTypeInfo(string guid, string type, int version) {
+    public class GuidTypeInfo : IComparable<GuidTypeInfo>
+    {
+        public GuidTypeInfo(string guid) : this(guid, "", 0) { }
+        public GuidTypeInfo(string guid, string type, int version)
+        {
             Guid = guid;
             TypeName = type;
             Version = version;
@@ -219,33 +252,43 @@ namespace PackFileEditorStandalone
         /*
          * Comparable (mostly to sort the master schema for easier version control).
          */
-        public int CompareTo(GuidTypeInfo other) {
+        public int CompareTo(GuidTypeInfo other)
+        {
             int result = TypeName.CompareTo(other.TypeName);
-            if (result == 0) {
+            if (result == 0)
+            {
                 result = Version - other.Version;
             }
-            if (result == 0) {
+            if (result == 0)
+            {
                 result = Guid.CompareTo(other.Guid);
             }
             return result;
         }
         #region Framework overrides
-        public override bool Equals(object obj) {
+        public override bool Equals(object obj)
+        {
             bool result = obj is GuidTypeInfo;
-            if (result) {
-                if (string.IsNullOrEmpty(Guid)) {
+            if (result)
+            {
+                if (string.IsNullOrEmpty(Guid))
+                {
                     result = (obj as GuidTypeInfo).TypeName.Equals(TypeName);
                     result &= (obj as GuidTypeInfo).Version.Equals(Version);
-                } else {
+                }
+                else
+                {
                     result = (obj as GuidTypeInfo).Guid.Equals(Guid);
                 }
             }
             return result;
         }
-        public override int GetHashCode() {
+        public override int GetHashCode()
+        {
             return Guid.GetHashCode();
         }
-        public override string ToString() {
+        public override string ToString()
+        {
             return string.Format("{1}/{2} # {0}", Guid, TypeName, Version);
         }
         #endregion
@@ -254,10 +297,13 @@ namespace PackFileEditorStandalone
     /*
      * Comparer for two guid info instances.
      */
-    class GuidInfoComparer : Comparer<GuidTypeInfo> {
-        public override int Compare(GuidTypeInfo x, GuidTypeInfo y) {
+    class GuidInfoComparer : Comparer<GuidTypeInfo>
+    {
+        public override int Compare(GuidTypeInfo x, GuidTypeInfo y)
+        {
             int result = x.TypeName.CompareTo(y.TypeName);
-            if (result == 0) {
+            if (result == 0)
+            {
                 result = y.Version - x.Version;
             }
             return result;
@@ -267,7 +313,8 @@ namespace PackFileEditorStandalone
     /*
      * Compares two versioned infos to best match a version being looked for.
      */
-    class BestVersionComparer : IComparer<TypeInfo> {
+    class BestVersionComparer : IComparer<TypeInfo>
+    {
         public int TargetVersion { get; set; }
         public int Compare(TypeInfo info1, TypeInfo info2)
         {
