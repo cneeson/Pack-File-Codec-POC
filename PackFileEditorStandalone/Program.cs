24a044379e7182c3b4391bda930bf8c36e98d65f
Craig Neeson <craigneeson@hotmail.co.uk>
2017-10-05 20:54:31 +0100

    Added experimental packed file editing wizardry that doesnt completely work.

--- PackFileEditorStandalone/Program.cs
+++ PackFileEditorStandalone/Program.cs
@@ -13,6 +13,9 @@ namespace PackFileEditorStandalone
         private static readonly string FILES_CURRENT_DIR = Directory.GetCurrentDirectory() + "\\Files\\";
         private static readonly string PACK_FILE = Directory.GetCurrentDirectory() + @"\Resources\unit_template.pack";
 
+        static UInt32 VERSION_MARKER = BitConverter.ToUInt32(new byte[] { 0xFC, 0xFD, 0xFE, 0xFF }, 0);
+        static UInt32 GUID_MARKER = BitConverter.ToUInt32(new byte[] { 0xFD, 0xFE, 0xFC, 0xFF }, 0);
+
         private static PackFile currentPackFile;        // Full pack file full of tables 
 
         static void Main(string[] args)
@@ -52,12 +55,75 @@ namespace PackFileEditorStandalone
         private static DataTable GetDataTable(String tableKey)
         {
             PackedFile currentPackedFile = currentPackFile.Files[0];
+            PackedFile replacementPackedFile = currentPackedFile;
 
-            // NEXT STEP, WORK OUT WAY TO GET SCHEMA.XMLs IN BECAUSE THAT IS WHY THERE IS CURRENTLY
-            // NO DBFile BEING PRODUCED
             DBFile test = PackedFileDbCodec.Decode(currentPackedFile);
 
+            test.Entries[0][0].Value = "dicks and or butts";
+
+            Encode(new MemoryStream(replacementPackedFile.Data), test);
+
+            //currentPackFile.Files[0] = currentPackedFile;
+
+            //currentPackFile.Files[0] = null;
+
+            currentPackFile.Files.Remove(currentPackedFile);
+            currentPackFile.Files.Add(replacementPackedFile);
+
             return new DataTable();
+        }
+
+        public static void Encode(Stream stream, DBFile file)
+        {
+            BinaryWriter writer = new BinaryWriter(stream);
+            file.Header.EntryCount = (uint)file.Entries.Count;
+            WriteHeader(writer, file.Header);
+            file.Entries.ForEach(delegate (DBRow e) { WriteEntry(writer, e); });
+            writer.Flush();
+        }
+        /*
+         * Encode db file to memory and return it as a byte array.
+         */
+        public static byte[] Encode(DBFile file)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                Encode(stream, file);
+                return stream.ToArray();
+            }
+        }
+
+        private static void WriteEntry(BinaryWriter writer, List<FieldInstance> fields)
+        {
+            for (int i = 0; i < fields.Count; i++)
+            {
+                try
+                {
+                    FieldInstance field = fields[i];
+                    field.Encode(writer);
+                }
+                catch (Exception x)
+                {
+                    Console.WriteLine(x);
+                    throw x;
+                }
+            }
+        }
+
+        public static void WriteHeader(BinaryWriter writer, DBFileHeader header)
+        {
+            if (header.GUID != "")
+            {
+                writer.Write(GUID_MARKER);
+                IOFunctions.WriteCAString(writer, header.GUID, Encoding.Unicode);
+            }
+            if (header.Version != 0)
+            {
+                writer.Write(VERSION_MARKER);
+                writer.Write(header.Version);
+            }
+            writer.Write((byte)1);
+            writer.Write(header.EntryCount);
         }
     }
 }
